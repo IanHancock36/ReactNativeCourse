6d023dee9c226cc932d83cd1e39c88c63ba5d8e1
Ian Hancock <ianhancock@DALML6774.local>
2022-08-15 16:09:44 -0500

    text input

--- App.js
+++ App.js
@@ -5,22 +5,42 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='The Goals' />
-        <Button title = "Add Goal"/>
-        </View>
-      <View>
-        <Text>List of the goals.....</Text>
-        </View>
+        <TextInput style={styles.textInput} placeholder='The Goals' />
+        <Button title="Add Goal" />
       </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of the goals.....</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding:50,
-   
+    flex:1,
+    padding: 50,
+    paddingHorizontal: 16,
+
   },
-  inputContainer:{
-    flexDirection:'row'
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 24,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    borderRadius: 10,
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 3
   }
 });
